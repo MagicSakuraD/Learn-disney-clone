85b2c02d793159ff242d2469804410f04eae35e7
MagicSakuraD <ckgoforit@outlook.com>
2023-04-15 23:36:16 +0800

    style content

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,29 @@
-import style from "styled-components";
+import styled from "styled-components";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <Container>
+      <Content>内容</Content>
+    </Container>
+  );
 };
 
+const Container = styled.section`
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  height: 100vh;
+`;
+
+const Content = styled.div`
+  margin-bottom: 10vw;
+  width: 100%;
+  position: relative;
+  min-height: 100vh;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 export default Login;
