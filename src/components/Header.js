e0296bf1a61208863a28de055dbde6621380e9f1
MagicSakuraD <ckgoforit@outlook.com>
2023-04-18 23:57:05 +0800

    add Nav logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,32 @@
 import styled from "styled-components";
 
 const Header = (props) => {
-  return <Nav>Header</Nav>;
+  return (
+    <Nav>
+      <Logo>
+        <img src="/images/logo.svg" alt="Disney+" />
+      </Logo>
+    </Nav>
+  );
 };
 
 const Nav = styled.nav`
   position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 70px;
+  background-color: #090b13;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 36px;
+  letter-spacing: 16px;
+  z-index: 3;
 `;
+
+const Logo = styled.logo`
+  padding: 0;
+  width: 80px;
+`;
+
 export default Header;
